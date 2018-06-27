0bb823cb37ed5fbc18bf179f1bf8e1a47f6b1ca3
Peiru.li@fujixerox.com <peiru@TudouDigua.local>
2018-06-27 14:54:31 +0800

    update

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
-import LabelInput from './common/LabelInput';
 
 const styles = StyleSheet.create({
   container: {
